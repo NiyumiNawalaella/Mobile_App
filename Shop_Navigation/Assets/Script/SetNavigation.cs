98e4c035f028810a8a277e91f3f959bde7df3f1f
NiyumiNawalaella <niyumi2100@gmail.com>
2023-05-18 12:52:09 +0530

    ar navigation added

--- Shop_Navigation/Assets/Script/SetNavigation.cs
+++ Shop_Navigation/Assets/Script/SetNavigation.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SetNavigation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Camera topDownCamera;
+    [SerializeField]
+    private GameObject navTargetObject;
+
+    private NavMeshPath path; //current calculated path
+    private LineRenderer line; //linerenderer o display path
+
+    private bool lineToggle = false;
+
+    private void Start()
     {
-        
+        path = new NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            lineToggle = !lineToggle;
+        }
+        if(lineToggle)
+        {
+            NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            line.SetPositions(path.corners);
+            line.enabled = true;
+        }
     }
+
 }
