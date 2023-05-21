9e24f0a9669c8f2adb32cacefaf8e00b1a72b3e2
NiyumiNawalaella <niyumi2100@gmail.com>
2023-05-22 03:22:48 +0530

    changes made

---
+++ Shop_Navigation/Assets/Script/TargetFacade.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFacade : MonoBehaviour
+{
+    public string Name;
+    public int FloorNumber;
+}
