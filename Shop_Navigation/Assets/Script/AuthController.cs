c969805c32fbd79afb21b09ceddc1ab5318e3229
NiyumiNawalaella <niyumi2100@gmail.com>
2023-05-23 12:11:41 +0530

    changes made

--- Shop_Navigation/Assets/Script/AuthController.cs
+++ Shop_Navigation/Assets/Script/AuthController.cs
@@ -10,12 +10,13 @@ public class AuthController : MonoBehaviour
 {
     public InputField emailInput, passwordInput, forgetPassEmail;
     public GameObject Login_Panel, ForgetPassordPanel, WarningPanel;
-    public Text warning_Title_Text, warning_Message_Text;
+    public Text warning_Title_Text, warning_Message_Text, Login_Text;
 
     [Header("Firebase")]
     public DependencyStatus dependencyStatus;
     public FirebaseUser User;
     public FirebaseAuth Auth;
+    //private FirebaseApp app;
 
     public void OpenLoginPanel()
     {
@@ -58,6 +59,73 @@ public class AuthController : MonoBehaviour
     public void CloseWarningMessage_Panel()
     {
         WarningPanel.SetActive(false);
+    }
+    private void Awake()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                //If they are avaliable Initialize Firebase
+                //app = Firebase.FirebaseApp.DefaultInstance;
+               Debug.Log("Setting up Firebase Auth");
+               //Set the authentication instance object
+               Auth = FirebaseAuth.DefaultInstance;
+            }
+            else
+            {
+                showWarningMessage("Could not resolve all Firebase dependencies: ", " " + dependencyStatus);
+            }
+        });
+    }
+    public void LoginButton()
+    {
+        //Call the login coroutine passing the email and password
+        StartCoroutine(Login(emailInput.text, passwordInput.text));
+    }
+
+    private IEnumerator Login(string _email, string _password)
+    {
+        //Call the function auth signin function passing the email and password
+        var LoginTask = FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(_email, _password);
+        yield return new WaitUntil(predicate: () => LoginTask.IsCompleted);
+
+        if(LoginTask.Exception != null)
+        {
+            //If there are errors handle them
+            Debug.LogWarning(message:$"Failed to register task with {LoginTask.Exception}");
+            FirebaseException firebaseEx = LoginTask.Exception.GetBaseException() as FirebaseException;
+            AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+            showWarningMessage("","Login Failed!");
+            switch(errorCode)
+            {
+                case AuthError.MissingEmail:
+                    showWarningMessage("", "Missing Email");
+                    break;
+                case AuthError.MissingPassword:
+                    showWarningMessage("", "Missing Password");
+                    break;
+                case AuthError.WrongPassword:
+                    showWarningMessage("", "Wrong Password");
+                    break;
+                case AuthError.InvalidEmail:
+                    showWarningMessage("", "Invalid Email");
+                    break;
+                case AuthError.UserNotFound:
+                    showWarningMessage("", "Account does not exist");
+                    break;
+            }
+        }
+        else
+        {
+            //User is now logged in
+            //Now get the result
+            User = LoginTask.Result.User;
+            Debug.LogFormat("User signed in successfully: {0} ({1})", User.UserId, User.Email);
+            Login_Text.text = "Logged In";
+        }
     }
 }
    /* public void SignInUser(string email, string password)
